00593361c79cd70a434e293db7be665a23be2ac5
Riya Patidar <riyapatidar0111@gmail.com>
2026-05-29 13:18:59 +0530

    Multiply is added to you branch

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class MultiplyExample {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int product = a * b; // Result is 50
+        
+        System.out.println("Product: " + product);
+    }
+}
